2fab4ab6cf0a10a8466412adbec494e18d716530
DenizcanSarigul <119871445+DenizcanSarigul@users.noreply.github.com>
2023-12-20 17:43:37 +0100

    1

--- Streamlit/FrenchApp.py
+++ Streamlit/FrenchApp.py
@@ -15,21 +15,9 @@ st.set_page_config(
     layout="wide",
 )
 
-# Custom HTML for background image
-background_html = """
-<style>
-body {
-    background-image: url("https://media.istockphoto.com/id/1209873889/de/foto/weinberg-saint-emilion-unter-der-herbstsonne.jpg?s=1024x1024&w=is&k=20&c=5CRoVOrksviM3yWCB4EfBem0bRXALKWK8VRNa0fWx4I=");
-    background-size: cover;
-    background-repeat: no-repeat;
-}
-</style>
-"""
 
 # Streamlit app layout
 def main():
-    # Inject custom HTML for background image
-    st.markdown(background_html, unsafe_allow_html=True)
 
     st.title("Text Difficulty Analyser and Reading Suggestions")
 
@@ -77,18 +65,74 @@ def display_reading_suggestions(difficulty):
     # Placeholder for reading suggestions
     if difficulty == 'A1':
         st.write("Reading suggestions for A1 level:")
+
+        book_links_A1 = {
+            "Merde, It's Not Easy to Learn French": "https://www.amazon.com/Merde-Its-Easy-Learn-French/dp/1795292733?crid=BBJ0F0HCMF82&keywords=merde+its+not+easy+to+learn+french&qid=1639658757&sprefix=merde+it%27s+not+easy,aps,224&sr=8-3&linkCode=sl1&tag=relearnalangu-20&linkId=46371e44e1932ab005474112eb8b2496&language=en_US&ref_=as_li_ss_tl",
+            "La planète grise": "https://www.thecibookshop.com/en/la-planete-grise.html",
+            "Semi Professionel": "https://www.thecibookshop.com/en/semi-professionnel-tome-1.html"
+        }
+
+        for title, link in book_links_A1.items():
+            st.markdown(f"[{title}]({link})")
+            
+    elif difficulty == 'A2':
+        st.write("Reading suggestions for A2 level:")
+        
+        book_links_A2 = {
+            "Mystère à Nice" : "https://www.blackcat-cideb.com/en/books/mystere-a-nice-en",
+            "Jeanne D'Arc": "https://www.blackcat-cideb.com/en/books/jeanne-d-arc-en",
+            "Aventure dans les Pyrénées": "https://www.blackcat-cideb.com/en/books/aventure-dans-les-pyrenees-en"
+        }
+
+        for title, link in book_links_A2.items():
+            st.markdown(f"[{title}]({link})")
+
     elif difficulty == 'B1':
         st.write("Reading suggestions for B1 level:")
-        # Display B1 reading suggestions
+        
+        book_links_B1 = {"Le tour du monde en 80 jours" : "https://www.blackcat-cideb.com/en/books/le-tour-du-monde-en-80-jours-5",
+                        "Voyage au centre de la Terre" : "https://www.blackcat-cideb.com/en/books/voyage-au-centre-de-la-terre-en",
+                        "Enquête à Saint-Malo": "https://www.blackcat-cideb.com/en/books/enquete-a-saint-malo-en"
+        }
+
+        for title, link in book_links_B1.items():
+            st.markdown(f"[{title}]({link})")
+
     elif difficulty == 'B2':
         st.write("Reading suggestions for B2 level:")
-        # Display B2 reading suggestions
+        
+        book_links_B2 = {
+            "Vengeance à La Réunion" : "https://www.blackcat-cideb.com/en/books/vengeance-a-la-reunion-en",
+            "Double assassinat dans la Rue Morgue et La lettre volée" : "https://www.blackcat-cideb.com/en/books/double-assassinat-dans-la-rue-morgue-et-la-lettre-volee-en",
+            "Le Mystère de la Chambre Jaune" : "https://www.blackcat-cideb.com/en/books/mystere-de-la-chambre-jaune-le-en"
+        }
+
+        for title, link in book_links_B2.items():
+            st.markdown(f"[{title}]({link})")
+
     elif difficulty == 'C1':
         st.write("Reading suggestions for C1 level:")
-        # Display C1 reading suggestions
+
+        book_links_C1 = {"L'Île mystérieuse" : "https://anylang.net/en/books/fr/mysterious-island",
+                         "Les Trois Mousquetaires" : "https://anylang.net/en/books/fr/three-musketeers",
+                         "Pilote de guerre": "https://anylang.net/en/books/fr/flight-arras"}
+        
+        for title, link in book_links_C1.items():
+            st.markdown(f"[{title}]({link})")
+
     elif difficulty == 'C2':
         st.write("Reading suggestions for C2 level:")
-        # Display C2 reading suggestions
+
+        book_links_C2 = {"Cyrano de Bergerac" : "https://anylang.net/en/books/fr/cyrano-de-bergerac",
+                         "A rebours" : "«Against Nature» in French with a Parallel Translation",
+                         "Les Fleurs du Mal" :  "https://anylang.net/en/books/fr/flowers-evil"
+
+        }
+
+        for title, link in book_links_C2.items():
+            st.markdown(f"[{title}]({link})")
 
 if __name__ == "__main__":
     main()
+
+    
